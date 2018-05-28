870336b2a4655d8cd88a94b6630d0e0fae547157
rcurnow1 <ryan.curnow@live.co.uk>
2018-05-28 11:12:14 +0100

    second inital commit

--- sketch.js
+++ sketch.js
@@ -6,7 +6,8 @@ function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.parent("banner");
   noStroke();
-  fill(129, 236, 236);
+  fill(223, 228, 234);
+
 }
 
 function windowResized() {
@@ -14,15 +15,15 @@ function windowResized() {
 }
 
 function draw() {
-  background(50, 30);
+  background(150, 150);
 
   for (let x = 0; x <= windowWidth; x = x + 70) {
     for (let y = 0; y <= windowHeight; y = y + 70) {
 
-      let xAngle = map(mouseX, 0, width, -0.5 * PI, 0.5 * PI, true);
-      let yAngle = map(mouseY, 0, height, -0.375 * PI, -0.375 * PI, true);
+      let xAngle = map(mouseX, 0, windowWidth, -0.5 * PI, 0.5 * PI, true);
+      let yAngle = map(mouseY, 0, windowHeight, -0.375 * PI, -0.375 * PI, true);
 
-      let angle = xAngle * (x / width) + yAngle * (y / height);
+      let angle = xAngle * (x / windowWidth) + yAngle * (y / windowHeight);
 
       let myX = x + 50 * cos(2 * PI * t + angle);
       let myY = y + 30 * sin(2 * PI * t + angle);
